1a785ee42442aed0dbd5177521f6796805920f3c
boby87 <fokoutghislain@gmail.com>
2025-08-11 16:55:49 +0400

    first

--- src/main/java/ktv/cm/idmate/dto/PhoneRequest.java
+++ src/main/java/ktv/cm/idmate/dto/PhoneRequest.java
@@ -8,7 +8,7 @@ public record PhoneRequest(
         @Pattern(regexp = "\\+\\d{1,4}", message = "Le code pays doit commencer par '+' et contenir entre 1 et 4 chiffres.")
         String countryCode,
         @NotBlank(message = "Le numéro de téléphone est requis.")
-        @Pattern(regexp = "\\d{6,15}", message = "Le numéro de téléphone doit contenir entre 6 et 15 chiffres.")
+       // @Pattern(regexp = "\\d{6,15}", message = "Le numéro de téléphone doit contenir entre 6 et 15 chiffres.")
         String  phoneNumber) {
 
 }
